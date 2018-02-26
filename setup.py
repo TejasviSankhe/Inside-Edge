d85412fdce50d05de5a5bbd964cc7dc571565927
TejasviSankhe <tejasvisankhe@gmail.com>
2018-02-26 02:10:34 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 import setuptools
 
-setuptools.setup(name='python_hiring_test')
+setuptools.setup(
+    name='PythonHiringTest',    
+    version='1.1',                          # Update the version number for new releases
+    packages=['python_hiring_test'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    author='TejasviSankhe',
+    long_description=open('README.md').read()
+    )
