7967e62ed05f37e2ebeca06c0e26f14ced924df2
Tejasvi Sankhe <tejasvisankhe@gmail.com>
2018-02-26 00:05:01 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
 import setuptools
 
-setuptools.setup(name='python_hiring_test')
+setuptools.setup(name='python_hiring_test_Tej',
+                packages = ['python_hiring_test'],
+                version = '0.0.1',
+                download_url = 'https://github.com/TejasviSankhe/Inside-Edge/archive/0.0.1.tar.gz',
+                keywords = ['inside-edge'])
